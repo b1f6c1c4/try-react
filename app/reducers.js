4a1dafaf4fc5018d671ec5e5b3fbb6f548be7867
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-19 22:55:15 +0800

    WIP: 3

--- app/reducers.js
+++ app/reducers.js
@@ -6,7 +6,6 @@ import { combineReducers } from 'redux-immutable';
 import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
-import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /*
@@ -43,7 +42,6 @@ function routeReducer(state = routeInitialState, action) {
 export default function createReducer(injectedReducers) {
   return combineReducers({
     route: routeReducer,
-    global: globalReducer,
     language: languageProviderReducer,
     ...injectedReducers,
   });
