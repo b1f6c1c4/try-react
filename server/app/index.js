e11521ea0475a89f4f87c26d4dcf953db2d73c07
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-19 13:42:30 +0800

    Use jwt

--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const nocache = require('nocache');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
 const router = express.Router();
 
+const JwtStrategy = passportJWT.Strategy;
+const ExtractJwt = passportJWT.ExtractJwt;
+const jwtOptions = {
+  issuer: 'try-react',
+  audience: 'try-react',
+};
+const pjwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 's3cReT',
+  ignoreExpiration: true,
+  jsonWebTokenOptions: jwtOptions,
+};
+
+passport.use(new JwtStrategy(pjwtOptions, (payload, done) => {
+  if (payload.id.length < 4) {
+    return done(new Error('User not exist'), null);
+  }
+
+  return done(null, { id: payload.id });
+}));
+
+router.use(nocache(), bodyParser.json(), passport.initialize());
+
 router.get('/', (req, res) => res.json({}));
+
+router.post('/login', (req, res) => {
+  const payload = { id: req.body.un };
+  const token = jwt.sign(payload, pjwtOptions.secretOrKey, jwtOptions);
+  res.json({
+    message: 'ok',
+    token,
+  });
+});
+
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res) => res.status(200).json(req.user));
 
 router.get('*', (req, res) => res.status(501).send());
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 /* eslint consistent-return:0 */
 
 const express = require('express');
-const session = require('express-session');
-const nocache = require('nocache');
-const Redis = require('ioredis');
-const RedisStore = require('connect-redis')(session);
 const logger = require('./logger');
 
 const argv = require('./argv');
@@ -16,29 +12,8 @@ const resolve = require('path').resolve;
 const api = require('./app');
 const app = express();
 
-const redis = new Redis(process.env.REDIS_URL || 'localhost:6379');
-
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-const sess = {
-  name: 'try-react',
-  secret: 'try-react',
-  store: new RedisStore({
-    client: redis,
-  }),
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-};
-if (process.env.NODE_ENV === 'production' &&
-    process.env.REDIECT_URL) {
-  app.set('trust proxy', 1);
-  sess.cookie.secure = true;
-}
-
-app.use('/api', nocache(), session(sess), api);
+app.use('/api', api);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
