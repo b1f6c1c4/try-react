ceb0dcd7efb4b45d0385a35a371963bed94fe67c
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-20 14:10:20 +0800

    WIP: 4

--- app/tests/i18n.test.js
+++ app/tests/i18n.test.js
@@ -1,5 +1,4 @@
-import { ROOT_LOCALE } from '../i18n';
-import { formatTranslationMessages } from '../i18n';
+import { ROOT_LOCALE, formatTranslationMessages } from '../i18n';
 
 jest.mock('../translations/en.json', () => (
   {
