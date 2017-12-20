0a582dc3a0fd16d0eca7b1511c7c1021e74d5786
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-20 23:49:12 +0800

    NotFoundPage: msg

--- app/containers/NotFoundPage/messages.js
+++ app/containers/NotFoundPage/messages.js
@@ -3,6 +3,10 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.containers.NotFoundPage.header',
-    defaultMessage: 'This is NotFoundPage container !',
+    defaultMessage: '404 Not Found',
+  },
+  returnHome: {
+    id: 'app.containers.NotFoundPage.returnHome',
+    defaultMessage: 'Click here to return home',
   },
 });
