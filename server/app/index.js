290010db23cde4dae62c690231371b7def47ff93
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-21 19:42:42 +0800

    Allow CORS

--- server/app/index.js
+++ server/app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const nocache = require('nocache');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
@@ -12,11 +13,13 @@ const ExtractJwt = passportJWT.ExtractJwt;
 const jwtOptions = {
   issuer: 'try-react',
   audience: 'try-react',
+  expiresIn: '2h',
 };
 const pjwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: 's3cReT',
-  ignoreExpiration: true,
+  ignoreExpiration: false,
+  maxAge: '2h',
   jsonWebTokenOptions: jwtOptions,
 };
 
@@ -28,12 +31,21 @@ passport.use(new JwtStrategy(pjwtOptions, (payload, done) => {
   return done(null, { id: payload.id });
 }));
 
+router.use(cors({
+  origin: '*',
+  methods: ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
+
 router.use(nocache(), bodyParser.json(), passport.initialize());
 
 router.get('/', (req, res) => res.json({}));
 
 router.post('/login', (req, res) => {
-  const payload = { id: req.body.un };
+  const payload = {
+    username: req.body.username,
+  };
   const token = jwt.sign(payload, pjwtOptions.secretOrKey, jwtOptions);
   res.json({
     message: 'ok',
