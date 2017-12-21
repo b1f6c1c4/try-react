ae9e2d302056d812879ee82d55f26998daff1977
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-21 18:57:58 +0800

    CI settings

--- internals/netlify.sh
+++ internals/netlify.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-zip -r website.zip build
+if [[ -z "$NETLIFYKEY" ]]; then
+  echo "Ignore deploying to Netlify"
+else
+  echo "Start deploying to Netlify..."
 
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $NETLIFYKEY" \
-     --data-binary "@website.zip" \
-     https://api.netlify.com/api/v1/sites/47b66880-6807-4399-bc94-91e327d44c46/deploys
+  zip -r website.zip build
 
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $NETLIFYKEY" \
-     --data-binary "@website.zip" \
-     https://api.netlify.com/api/v1/sites/3bb810a7-9b7e-4af9-b1fe-eddffe55f3f2/deploys
+  curl -H "Content-Type: application/zip" \
+       -H "Authorization: Bearer $NETLIFYKEY" \
+       --data-binary "@website.zip" \
+       https://api.netlify.com/api/v1/sites/47b66880-6807-4399-bc94-91e327d44c46/deploys
+
+  curl -H "Content-Type: application/zip" \
+       -H "Authorization: Bearer $NETLIFYKEY" \
+       --data-binary "@website.zip" \
+       https://api.netlify.com/api/v1/sites/3bb810a7-9b7e-4af9-b1fe-eddffe55f3f2/deploys
+fi
