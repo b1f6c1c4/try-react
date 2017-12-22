6c0a7a6f2c6de347ffa07409f51e4a1742b78d5a
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-12-22 14:05:24 +0800

    Remove frontend

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -4,16 +4,8 @@
  * Front-end middleware
  */
 module.exports = (app, options) => {
-  const isProd = process.env.NODE_ENV === 'production';
-
-  if (isProd) {
-    const addProdMiddlewares = require('./addProdMiddlewares');
-    addProdMiddlewares(app, options);
-  } else {
-    const webpackConfig = require('../../internals/webpack/webpack.dev.babel');
-    const addDevMiddlewares = require('./addDevMiddlewares');
-    addDevMiddlewares(app, webpackConfig);
-  }
+  const addProdMiddlewares = require('./addProdMiddlewares');
+  addProdMiddlewares(app, options);
 
   return app;
 };
